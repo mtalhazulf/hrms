c8b0d256074120ba5359e433e03231e035ac65bd
Muhammad Hassan Jawwad <hassanjawwad01@gmail.com>
2024-01-08 12:39:03 +0500

    fixed team

--- src/Teams/Template.jsx
+++ src/Teams/Template.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 const Template = () => {
 
   const navigate = useNavigate();
-  const heading = ["Project Name", "Team Lead", "Deadline", "Milestone", "Description"];
+  const heading = ["Project Name", "Members"];
 
   const [Project, setProject] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
